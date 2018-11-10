19d67410f691402cc831481e98dcaa7c1fa03c38
markomijaljevic <markomijaljevic96@gmail.com>
2018-11-10 12:02:09 +0100

    camelcase json formatter

--- MovieRentalStore/Dtos/CustomerDto.cs
+++ MovieRentalStore/Dtos/CustomerDto.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using MovieRentalStore.Models;
 
 namespace MovieRentalStore.Dtos
 {
     public class CustomerDto
     {
+        public int Id { get; set; }
 
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        [Min18YearsOld]
+        public DateTime? Birthdate { get; set; }
+
+        public byte MembershipTypeId { get; set; }
     }
 }
